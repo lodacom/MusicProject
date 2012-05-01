14caf751aa41f334e28278fd724ba1ebd9e63a25
lodacom <loda-compagnie@hotmail.com>
2012-05-01 15:57:42 +0200

    Meilleur accueil

--- Views/pages/accueil.php
+++ Views/pages/accueil.php
@@ -1,38 +1,17 @@
+﻿<?php
+	session_start();
+?>
 <html>
 <head> 
-<script src="../../Library/jquery.js" type="text/javascript"></script>
-        <script type="text/javascript">
-            $(document).ready(function(){
-
-                $("#Connexion").click(function(){
-                    if ($("#Formulaire_Connexion").is(":hidden")){
-                        $("#Formulaire_Connexion").slideDown("slow");
-						$("#Formulaire_Connexion").css({zIndex: 1000});
-                    }
-                    else{
-                        $("#Formulaire_Connexion").slideUp("slow");
-                    }
-                });
-                
-            });
-            
-            function closeForm(){
-                $("#message_connexion").show("slow");
-                setTimeout('$("#message_connexion").hide();$("#contactForm").slideUp("slow")', 2000);
-				setTimeout('$("#Formulaire_Connexion").slideUp("slow")',1000);
-
-           }
-		   function closeForm2(){
-                $("#message_inscription").show("slow");
-                setTimeout('$("#message_inscription").hide();$("#contactForm").slideUp("slow")', 2000);
-				setTimeout('$("#Formulaire_Connexion").slideUp("slow")',1000);
-           }
-        </script>
+	<script src="../../Library/jquery.js" type="text/javascript"></script>
+	<script src="../include/slide_up_down.js" type="text/javascript"></script>
 	<link rel="stylesheet" href="../style/style_accueil.css"/>
 	<title> Accueil </title> 
 </head>
 <body>
-<?php include_once "../include/banniere_menu.php" ?>
+<?php 
+include_once "../include/banniere_menu.php"; 
+?>
 <div id="conteneur">
 	<div id="menu_gauche">
 	<span class="menu">Accueil</span>
