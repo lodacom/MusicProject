0713ad7fa46fa4152ed6f537af3aa63044e13b07
lodacom <loda-compagnie@hotmail.com>
2012-05-01 15:45:53 +0200

    classe mere + coup de coeur

---
+++ Models/coup_coeur_musique.php
@@ -0,0 +1,35 @@
+﻿<?php
+include ('interface_util.php');
+
+class Coup_Coeur_Musique extends Interface_Util
+{
+	
+	function __construct()
+	{
+		parent::__construct();
+	}
+	
+	function affichage_coup_coeur_musique
+	{
+		session_start();
+		if (isset($_SESSION['pseudo'])
+		{
+			$util_pres=$_SESSION['pseudo'];
+			$requete="SELECT M.num_musique,M.titre,A.nom,M.duree,G.nom_groupe
+			FROM Coup_Coeur_Musique C, Musique M, Album A
+			WHERE pseudo='$util_pres'
+			AND C.num_musique=M.num_musique
+			AND M.num_album=A.num_album
+			AND A.nom_groupe=G.nom_grouper";
+	
+			$resultat=mysql_query($requete,parent::connexion->link);
+			
+			parent::interface_commune($resultat);
+		}
+		else
+		{
+			echo ("Vous n'etes pas connecte, nous ne pouvons donc afficher votre panier\n");
+		}
+	}
+}
+?>
---
+++ Models/interface_util.php
@@ -0,0 +1,50 @@
+﻿<?php
+include ('connexion_base.php');
+class Interface_Util
+{
+	public $connexion=null;
+	public $tab_num_titre=null;
+	public $tab_titre=null;
+	public $tab_album=null;
+	public $tab_duree=null;
+	public $tab_groupe=null;
+	
+	function __construct()
+	{
+		$this->connexion=new Connexion_Base();
+		$this->tab_num_musique=array();//sera en hidden pour pouvoir supprimer
+		$this->tab_titre=array();
+		$this->tab_album=array();
+		$this->tab_duree=array();
+		$this->tab_groupe=array();
+	}
+	
+	function interface_commune($p_resultat)
+	{
+		if(!$p_resultat)
+			{
+				echo ("Vous n'avez rien mis dans votre panier\n");
+			}
+			else
+			{
+				for ($i=0;$i<mysql_num_rows($p_resultat);$i++)
+				{
+					array_push($this->tab_num_musique,
+					mysql_result($p_resultat,$i,'M.num_musique'));
+					
+					array_push($this->tab_titre,
+					mysql_result($p_resultat,$i,'M.titre'));
+					
+					array_push($this->tab_album,
+					mysql_result($p_resultat,$i,'A.nom'));
+					
+					array_push($this->tab_duree,
+					mysql_result($p_resultat,$i,'M.duree');
+					
+					array_push($this->tab_groupe,
+					mysql_result($p_resultat,$i,'G.nom_groupe');
+				}
+			}
+	}
+}
+?>
