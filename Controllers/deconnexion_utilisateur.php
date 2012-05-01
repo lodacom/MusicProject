a17e3c49a49765140daf67c7daa49de994f1ccb2
lodacom <loda-compagnie@hotmail.com>
2012-05-01 15:42:44 +0200

    Pour la deconnexion du user ne fonctionne chai pas pk??

---
+++ Controllers/deconnexion_utilisateur.php
@@ -0,0 +1,12 @@
+﻿<?php
+class Deconnexion_Utilisateur
+{
+	function __construct()
+	{
+		unset($_SESSION['pseudo']);
+		session_destroy();
+		header("location: ../index.php");
+	}
+}
+$disconnect=new Deconnexion_Utilisateur();
+?>
