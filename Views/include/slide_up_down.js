128d3905d47cdc7af463a179d2d49c9bf3b5f64e
lodacom <loda-compagnie@hotmail.com>
2012-05-01 15:56:07 +0200

    Penser a faire un max d'include pour rendre lisible le code

---
+++ Views/include/slide_up_down.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function()
+{
+	$("#Connexion").click(function()
+	{
+        if ($("#Formulaire_Connexion").is(":hidden"))
+		{
+            $("#Formulaire_Connexion").slideDown("slow");
+			$("#Formulaire_Connexion").css({zIndex: 1000});
+		}
+        else
+		{
+			$("#Formulaire_Connexion").slideUp("slow");
+        }
+    });
+});
+            
+function closeForm()
+{
+    $("#message_connexion").show("slow");
+    setTimeout('$("#message_connexion").hide();$("#contactForm").slideUp("slow")', 2000);
+	setTimeout('$("#Formulaire_Connexion").slideUp("slow")',1000);
+}
+
+function closeForm2()
+{
+    $("#message_inscription").show("slow");
+    setTimeout('$("#message_inscription").hide();$("#contactForm").slideUp("slow")', 2000);
+	setTimeout('$("#Formulaire_Connexion").slideUp("slow")',1000);
+}
