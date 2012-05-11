93baeef8ca662d60b16b3d8820c2a470f8a31802
lodacom <loda-compagnie@hotmail.com>
2012-05-11 10:53:00 +0200

    ajout/supp pour coup_coeur

---
+++ Controllers/ajout_coup_coeur.php
@@ -0,0 +1,43 @@
+﻿<?php
+include ('../Models/connexion_base.php');
+class Ajout_Coup_Coeur
+{
+	private $connexion=null;
+	private $num_musique=null;
+	
+	function __construct()
+	{
+		$this->connexion=new Connexion_Base();
+		$this->num_musique=$_POST['num_musique'];
+	}
+	
+	function ajout_coup_coeur()
+	{
+		session_start();
+		if (isset($_SESSION['pseudo'])
+		{
+			$util_pres=$_SESSION['pseudo'];
+			$requete=("INSERT INTO Coup_Coeur_Musique
+			(pseudo,num_musique) 
+			VALUES ('$util_pres',$this->num_musique)");
+		
+			$resultat=mysql_query($requete,$this->connexion->link);
+			if (!$resultat)
+			{
+				echo("Oups erreur d'insertion!\n");
+			}
+			else
+			{
+				header("../Models/coup_coeur_musique.php");
+			}
+		}
+		else
+		{
+			echo("Veuillez vous connecter, nous ne pouvons ajouter
+			vos musiques à votre panier\n");
+		}
+	}
+}
+$ajout_coup_coeur=new Ajout_Coup_Coeur();
+$ajout_coup_coeur->ajout_coup_coeur();
+?>
---
+++ Controllers/supp_coup_coeur.php
@@ -0,0 +1,35 @@
+﻿<?php
+include ('../Models/connexion_base.php');
+class Supp_Coup_Coeur
+{
+	private $connexion=null;
+	private $num_musique=null;
+	private $util_pres=null;
+	
+	function __construct()
+	{
+		$this->connexion=new Connexion_Base();
+		$this->util_pres=$_SESSION['pseudo'];
+		$this->num_musique=$_POST['num_musique'];
+	}
+	
+	function suppression_coup_coeur_musique()
+	{
+		$requete=("DELETE FROM Coup_Coeur_Musique
+		WHERE pseudo='$this->util_pres'
+		AND num_musique=$this->num_musique");
+		
+		$resultat=mysql_query($requete,$this->connexion->link);
+		if (!$resultat)
+		{
+			echo("Oups erreur de suppression!\n");
+		}
+		else
+		{
+			header("../Models/coup_coeur_musique.php");
+		}
+	}
+}
+$supp_coup_coeur=new Supp_Coup_Coeur();
+$supp_coup_coeur->suppression_coup_coeur_musique();
+?>
